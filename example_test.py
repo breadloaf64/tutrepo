92fb20922368c0bb2e4c559b4daab687ab77cf99
Peter Marsh <breadloaf64@gmail.com>
2022-02-17 09:21:59 +0000

    added tests

--- example_test.py
+++ example_test.py
@@ -1,8 +1,8 @@
 def avg(a, b):
     return (a + b) / 2
 
-# def test_fail():
-#     assert avg(3,5) == 3
+def test_fail():
+    assert avg(3,5) == 3
 
-# def test_succ():
-#     assert avg(3,5) == 4
+def test_succ():
+    assert avg(3,5) == 4
